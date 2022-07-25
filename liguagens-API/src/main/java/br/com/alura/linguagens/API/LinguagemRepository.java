2c593483df49cfb56881e11cd0f1123e19dfd694
L_petillo <102457266+L-petillo@users.noreply.github.com>
2022-07-25 16:22:14 -0300

    Delete liguagens-API directory

--- liguagens-API/src/main/java/br/com/alura/linguagens/API/LinguagemRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.alura.linguagens.API;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LinguagemRepository extends MongoRepository<Linguagem, String>{
-    
-}
